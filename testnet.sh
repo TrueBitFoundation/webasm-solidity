aa55c450adcf43a38ea7334f376daa60c9bb7c93
Sami Mäkelä <mrsmkl@gmail.com>
2017-10-28 14:58:01 +0300

    added script for connecting to the test net

---
+++ testnet.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+service apache2 restart
+
+parity --chain kovan --unlock=`cat /myaddress` --password /supersecret.txt --ws-hosts=all --ws-origins=all &
+ipfs daemon &
+
+node setup.sh
+node app.js
+
