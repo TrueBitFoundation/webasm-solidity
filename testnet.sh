8aa8abf560b93d94cc8728e210d0f5f340d6824a
Sami Mäkelä <mrsmkl@gmail.com>
2017-10-28 15:24:03 +0300

    trying to fix

--- testnet.sh
+++ testnet.sh
@@ -5,6 +5,6 @@ service apache2 restart
 parity --chain kovan --unlock=`cat /myaddress` --password /supersecret.txt --ws-hosts=all --ws-origins=all &
 ipfs daemon &
 
-node setup.js
+node setup.js > config.json
 node app.js
 
