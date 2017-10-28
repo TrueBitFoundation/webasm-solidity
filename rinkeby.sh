cd4778c97a30daf6133c3998db27bb97cc79510a
Sami Mäkelä <mrsmkl@gmail.com>
2017-10-28 22:45:51 +0300

    waiting for geth to start up

--- rinkeby.sh
+++ rinkeby.sh
@@ -11,6 +11,8 @@ service apache2 restart
 geth --rinkeby --unlock 0 --password=supersecret.txt --ws --wsaddr 0.0.0.0 -wsapi eth --wsorigins="*" &
 ipfs daemon &
 
+sleep 10
+
 node setup.js rinkeby.json > config.json
 node app.js
 
