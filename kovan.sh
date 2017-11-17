d0e95eb5a29fd1c9646840ac37fe806b4d7292c5
Sami Mäkelä <mrsmkl@gmail.com>
2017-11-17 16:39:10 +0200

    still need some work

--- kovan.sh
+++ kovan.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
+echo plort > supersecret.txt
 mkdir -p /root/.local/share/io.parity.ethereum/chains/kovan/
 if [ ! -f /root/.local/share/io.parity.ethereum/chains/kovan/myaddress ]
 then
-  echo plort > supersecret.txt
   parity --chain kovan account new --password=supersecret.txt > /root/.local/share/io.parity.ethereum/chains/kovan/myaddress
 fi
 
