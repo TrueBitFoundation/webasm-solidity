f8088223eea431df576a667e38f2c331b15f98c6
Sami Mäkelä <mrsmkl@gmail.com>
2018-02-05 18:40:18 -0800

    thinking about how to implement a tournament for verification games

---
+++ contracts/match.sol
@@ -0,0 +1,41 @@
+
+pragma solidity ^0.4.19;
+
+// how to match people who disagree against each other
+
+contract Match {
+   
+   address[] agree;
+   bytes32 current;
+   
+   mapping (address => bytes32) vote;
+   
+   struct Game {
+      address fst;
+      address snd;
+      uint status;
+   }
+   
+   Game[] opponents;
+
+   // Add to the pool of people who agree
+   function addAgree(bytes32 cur) public {
+      require(cur == current || agree.length == 0);
+      vote[msg.sender] = current;
+      if (current != cur) current = cur;
+      agree.push(msg.sender);
+   }
+   
+   event StartGame(address a, address b);
+   
+   function addDisagree(bytes32 v) public {
+      require(current != v && agree.length != 0);
+      vote[msg.sender] = current;
+      address a = agree[agree.length-1];
+      agree.length--;
+      opponents.push(Game(msg.sender, a, 0));
+      StartGame(msg.sender, a);
+   }
+   
+}
+
