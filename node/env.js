60805e6e238d0054aadc456e6f612e8d37e46e4e
Sami Mäkelä <mrsmkl@gmail.com>
2018-04-17 14:21:21 +0300

    jit runtime works for scrypt example

--- node/env.js
+++ node/env.js
@@ -42,6 +42,48 @@ function allocArgs(m, lst) {
 
 var module
 
+var system = 0
+
+var info = JSON.parse(fs.readFileSync("globals.json"))
+
+var DYNAMICTOP_PTR = info.env.DYNAMICTOP_PTR
+
+var HEAP32, HEAP8
+
+function _sbrk(increment) {
+    console.log("sbrk", increment)
+      increment = increment|0;
+      var oldDynamicTop = 0;
+      var oldDynamicTopOnChange = 0;
+      var newDynamicTop = 0;
+      var totalMemory = 0;
+    
+    console.log(DYNAMICTOP_PTR, HEAP32[DYNAMICTOP_PTR>>2])
+    
+      oldDynamicTop = HEAP32[DYNAMICTOP_PTR>>2]|0;
+      newDynamicTop = oldDynamicTop + increment | 0;
+/*
+      if (((increment|0) > 0 & (newDynamicTop|0) < (oldDynamicTop|0)) // Detect and fail if we would wrap around signed 32-bit int.
+        | (newDynamicTop|0) < 0) { // Also underflow, sbrk() should be able to be used to subtract.
+        abortOnCannotGrowMemory()|0;
+        ___setErrNo(12);
+        return -1;
+      }
+*/
+      HEAP32[DYNAMICTOP_PTR>>2] = newDynamicTop;
+    /*
+      totalMemory = getTotalMemory()|0;
+      if ((newDynamicTop|0) > (totalMemory|0)) {
+        if ((enlargeMemory()|0) == 0) {
+          HEAP32[DYNAMICTOP_PTR>>2] = oldDynamicTop;
+          ___setErrNo(12);
+          return -1;
+        }
+      }
+      */
+      return oldDynamicTop|0;
+}
+
 // Make our runtime environment for the wasm module
 function makeEnv(env) {
     function finalize() {
@@ -53,12 +95,32 @@ function makeEnv(env) {
         finalize()
         process.exit(0)
     }
-    env.debugString = function (ptr) {}
-    env.debugBuffer = function (ptr, len) {}
-    env.debugInt = function (i) { console.log(i) }
+    env._sbrk = _sbrk
+    env._getSystem = function () { return system }
+    env._setSystem = function (ptr) { system = ptr }
+    env._debugSeek = function (ptr) {}
+    env._debugString = function (ptr) {
+        var str = ""
+        while (HEAP8[ptr] != 0) {
+            str += String.fromCharCode(HEAP8[ptr])
+            ptr++
+        }
+        console.log("DEBUG:", str)
+    }
+    env._debugBuffer = function (ptr, len) {
+        var str = ""
+        while (len > 0) {
+            str += String.fromCharCode(HEAP8[ptr])
+            len--
+            ptr++
+        }
+        console.log("DEBUG:", str)
+    }
+    env._debugInt = function (i) { console.log(i) }
     
     env._inputName = function (i,j) {
-        return input.name[i][j] || 0
+        // console.log("input name", i, j, input.name[i][j])
+        return input.name[i].charCodeAt(j) || 0
     }
     
     env._inputSize = function (i,j) {
@@ -66,13 +128,14 @@ function makeEnv(env) {
     }
     
     env._inputData = function (i,j) {
+        // console.log("input data", i, j, input.data[i][j])
         return input.data[i][j]
     }
     
     env._outputName = function (i,j,c) {
         var len = Math.max(input.name[i].length, j)
         var buf = Buffer.alloc(len, input.name[i])
-        console.log("doing output")
+        // console.log("doing output")
         input.name[i] = buf
         input.name[i][j] = c
     }
@@ -85,20 +148,31 @@ function makeEnv(env) {
         input.data[i][j] = c
     }
     
+}
+
+// var dta = JSON.parse(fs.readFileSync("info.json"))
+
+function handleImport(env, imp) {
+    if (imp.kind != "function") return
+    var str = imp.name
+    if (env[str]) return
     function makeDynamicCall(i) {
-        return function () { module["dynCall"+i].apply(null, arguments) }
+        return function () {
+            // console.log("dyncall", i)
+            return module["_dynCall"+i].apply(null, arguments)
+        }
     }
 
     // how to handle invokes? probably have to find all dynCalls
-    for (var i in env) {
-        if (i.substr(0,7) == "dynCall") {
-            env["invoke" + i.substr(7)] = makeDynamicCall(i)
-        }
+    if (str.substr(0,7) == "_invoke") {
+        var idx = str.substr(7)
+        env["_invoke" + idx] = makeDynamicCall(idx)
+        return
     }
     
+    console.log("should generate import", str)
+    env[str] = function () { console.log("called", str) }
 }
-
-var dta = JSON.parse(fs.readFileSync("info.json"))
 
 async function run(binary, args) {
     var info = { env: {}, global: {NaN: 0/0, Infinity:1/0} }
@@ -107,34 +181,54 @@ async function run(binary, args) {
     info.env.table = new WebAssembly.Table({ 'initial': 10, 'maximum': 10, 'element': 'anyfunc' });
     info.env.memory = new WebAssembly.Memory({ 'initial': sz, 'maximum': sz })
     
-    dta.map(e => { info[e[0]][e[1]] = function () {} })
+    // dta.map(e => { info[e[0]][e[1]] = function () {} })
     
+    var mod = await WebAssembly.compile(new Uint8Array(binary))
+    
+    var imports = WebAssembly.Module.imports(mod)
+    // console.log(imports)
+
     makeEnv(info.env)
     
+    imports.forEach(imp => handleImport(info.env,imp))
+    
     var m = await WebAssembly.instantiate(new Uint8Array(binary), info)
+    module = m.instance.exports
     
     m.wasmMemory = info.env.memory
+    
+    HEAP32 = new Uint32Array(info.env.memory.buffer)
+    HEAP8 = new Uint8Array(info.env.memory.buffer)
     
     var e = m.instance.exports
     
     // After building the environment, run the init functions
+    console.log("initializing")
+    if (e._initSystem) e._initSystem()
     if (e.__GLOBAL__I_000101) e.__GLOBAL__I_000101()
     if (e.__GLOBAL__sub_I_iostream_cpp) e.__GLOBAL__sub_I_iostream_cpp()
-    if (e._initSystem) e._initSystem()
-    
     var argv = allocArgs(m, args)
+    
+    console.log("calling main")
 
     e._main(args.length, argv)
 
-    if (m._finalizeSystem) e._finalizeSystem()
+    if (e._finalizeSystem) {
+        console.log("finalize")
+        e._finalizeSystem()
+    }
     
+    console.log("exiting")
+
     for (var i = 0; i < input.data.length; i++) {
         if (input.data[i].length > 0) {
-            fs.writeFileSync(input.name[i], input.data[i])
+            fs.writeFileSync(input.name[i]+".out", input.data[i])
         }
     }
     
 }
+
+process.argv.slice(2).forEach(loadFile)
 
 loadedFiles()
 
