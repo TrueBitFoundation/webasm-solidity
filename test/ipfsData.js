f508674dcdd8b074ff09ef06b07a8865fe42af4b
Sami Mäkelä <mrsmkl@gmail.com>
2018-06-06 18:54:15 +0300

    getting the ipfs block data to json file

---
+++ test/ipfsData.js
@@ -0,0 +1,29 @@
+
+const ipfsAPI = require('ipfs-api')
+const fs = require("fs")
+
+var ipfshost = "programming-progress.com"
+
+var ipfs = ipfsAPI(ipfshost, '5001', {protocol: 'http'})
+
+var obj = {}
+
+async function info(hash) {
+    console.log("Checking", hash)
+    var bl = await ipfs.block.get(hash)
+    console.log("Data", bl.data.length, "bytes:", bl.data.toString("hex").substr(0,100))
+    obj[hash] = bl.data.toString("hex")
+    var links = await ipfs.object.links(hash)
+    for (var i = 0; i < links.length; i++) {
+        var hash = links[i].toJSON().multihash
+        await info(hash)
+    }
+}
+
+async function main(hash) {
+    await info(hash)
+    fs.writeFileSync("custom.json", JSON.stringify(obj))
+}
+
+main(process.argv[2])
+
