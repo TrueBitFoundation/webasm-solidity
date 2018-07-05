71a3650b9af401b26a4388d808de395ed8affe08
Sami Mäkelä <mrsmkl@gmail.com>
2018-07-05 21:12:24 +0300

    calculating merkle root shoudl now work

--- merkle-computer/merkleRoot.js
+++ merkle-computer/merkleRoot.js
@@ -3,14 +3,14 @@ const zeroWord = Buffer.alloc(16)
 function makeMerkle(web3, arr, i, level) {
     if(level == 0) {
 	if (i < arr.length) {
-	    return arr[i]
+	    return "0x"+arr[i].toString("hex")
 	} else {
-	    return zeroWord
+	    return "0x"+zeroWord.toString("hex")
 	}
     } else {
 	return web3.utils.soliditySha3(
-	    makeMerkle(web3, arr, i, level-1),
-	    makeMerkle(web3, arr, i + Math.pow(2, level - 1), level-1)
+	    {t:"bytes", v:makeMerkle(web3, arr, i, level-1)},
+	    {t:"bytes", v:makeMerkle(web3, arr, i + Math.pow(2, level - 1), level-1)}
 	)
     }
 }
@@ -19,7 +19,7 @@ function depth(x) {
     if (x <= 1) {
 	return 0
     } else {
-	return 1 + depth(x/2)
+	return 1 + depth(Math.floor(x/2))
     }
 }
 
@@ -42,5 +42,6 @@ module.exports = (web3, input) => {
     let chunks = to16BytesArray(input)
     if (chunks.length < 1) chunks.push(zeroWord)
     if (chunks.length < 2) chunks.push(zeroWord)
-    return makeMerkle(web3, chunks, 0, depth(chunks.length * 2 - 1))
+    var res = makeMerkle(web3, chunks, 0, depth(chunks.length * 2 - 1))
+    return res
 }
