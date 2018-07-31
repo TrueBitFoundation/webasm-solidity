fc61aa7385804259820554c5e6d1a7b05db3933f
hswick <hdswick@gmail.com>
2018-07-31 15:18:02 -0400

    Added IDisputeResolutionLayer.sol interface

---
+++ contracts/IDisputeResolutionLayer.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.18;
+
+contract IDisputeResolutionLayer {
+    enum Status { NONE, UNRESOLVED, SOLVER_WINS, VERIFIER_WINS }
+    function status(bytes32 gameId) public view returns (Status);
+    function newGame(address solver, address verifier, bytes32 spec) public returns (bytes32 gameId);
+}
+
